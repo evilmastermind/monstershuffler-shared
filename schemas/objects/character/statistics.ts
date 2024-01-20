7af085ed154cf2e3de52669851a4307c38150bd6
Ismaele Gobbo <ismaele.gobbo@silla.industries>
2024-01-20 09:37:44 +0100

    value, valueAsWord

--- schemas/objects/character/statistics.ts
+++ schemas/objects/character/statistics.ts
@@ -37,7 +37,7 @@ export const abilitiesObject = z.object({
 });
 
 export const additionalStringTypes = z.enum([
-  'text', 'translatableText', 'nextLine', 'endOfParagraph', 'numberAsWord', 'ordinal', 'rollableNumberWithSign', 'feet', '-feet', 'ft', 'range/rangeMax', 'range', 'reach', 'resource', 'tag', 'rollableDice', 'rollableDiceDamage', 'rollableDiceDamageEnchantment'
+  'text', 'translatableText', 'nextLine', 'endOfParagraph', 'numberAsWord', 'ordinal', 'rollableNumberWithSign', 'feet', '-feet', 'ft', 'range/rangeMax', 'range', 'reach', 'resource', 'tag', 'value', 'valueAsWord', 'rollableValue'
 ]);
 
 // these are TailwindCSS classes
